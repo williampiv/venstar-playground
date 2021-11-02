0a4b2938ae15bcc16954357daa7927148c362101
William Perdue <wlperdue@perdueweb.com>
2021-11-02 10:51:10 -0700

    Add get for full thermostat data

--- sensor.py
+++ sensor.py
@@ -6,5 +6,15 @@ def get_sensor_readings(thermostat_ip: str) -> list:
         req.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print(e)
-        return {}
+        return []
     return req.json()['sensors']
+
+get_thermostat_info(thermostat_ip: str) -> dict:
+    req = requests.get)'http://{}/query/info'.format(thermostat_ip))
+    try:
+        req.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return {}
+    return req.json()
+
